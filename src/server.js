34b6385a0153ef3b28d136e107a46a64fe112b8a
DarrenWilliamson <darren.williamson95@yahoo.co.uk>
2018-05-02 01:06:08 +0100

    W/ Chat @ side

--- src/server.js
+++ src/server.js
@@ -385,17 +385,6 @@ var Server = (function(){
 
     io.sockets.on('connection', function(socket){
 
-        socket.on('new user', function(data, callback){
-            if (nicknames.indexOf(data) != -1){
-                callback(false);
-            } else {
-                callback(true);
-                socket.nickname = data;
-                nicknames.push(socket.nickname);
-                io.sockets.emit('usernames', nicknames);
-            }
-        });
-
         socket.on('send message', function(data){
             io.sockets.emit('new message', {msg: data, nick: socket.nickname});
         });
